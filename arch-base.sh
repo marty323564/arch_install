ed93425a4bb822272c0ae8710bda1628bfe9fc4d
Marty <marty323564@gmail.com>
2026-02-17 14:04:35 +0100

    Update arch-base.sh

--- arch-base.sh
+++ arch-base.sh
@@ -37,3 +37,9 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
+
+exit
+
+umount -R /mnt
+
+reboot
